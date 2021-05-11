6eb6efe5954702e0d37b48c3b746cde7bf78099e
planeset94 <planeset94@gmail.com>
2021-05-12 00:01:16 +0200

    Snack 2

--- asset/JS/main.js
+++ asset/JS/main.js
@@ -6,6 +6,7 @@ peso.
 Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
 */
 
+/*
 let bike = [
 
     {
@@ -38,4 +39,80 @@ let bikeEl = document.getElementById('bike');
 bikeEl.innerHTML = `
 <ul>La bici più leggera pesa: ${pesoMinore} Kg.</ul>
 `;
+
+*/
+
+/*
+Snack2:
+Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà:
+nome,punti fatti,falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+
+Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
+
+*/
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const clubs = [
+
+    {
+        nome: 'Milan',
+        punti: 0,
+        falli: 0,
+    },
+
+    {
+        nome: 'Juve',
+        punti: 0,
+        falli: 0,
+    },
+
+    {
+        nome: 'Inter',
+        punti: 0,
+        falli: 0,
+    },
+
+    {
+        nome: 'Como',
+        punti: 0,
+        falli: 0,
+    },
+
+    {
+        nome: 'Liverpool',
+        punti: 0,
+        falli: 0,
+    },
+
+    {
+        nome: 'Varese',
+        punti: 0,
+        falli: 0,
+    },
+
+
+];
+
+
+for (let i = 0; i < clubs.length; i++) {
+    clubs[i].punti = randomNumber(200, 1000);
+    clubs[i].falli = randomNumber(0, 500);
+
+};
+
+console.log(clubs);
+
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+let newClubs = [];
+for (let z = 0; z < clubs.length; z++) {
+    let { nome, falli } = clubs[z];
+    newClubs.push({ nome, falli });
+
+};
+console.log(newClubs);
 
