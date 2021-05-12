5afd4089102908def89d1c9fb34053cc916f2b39
planeset94 <planeset94@gmail.com>
2021-05-12 15:45:41 +0200

    SNACK 3

--- asset/JS/main.js
+++ asset/JS/main.js
@@ -40,6 +40,18 @@ bikeEl.innerHTML = `
 <ul>La bici più leggera pesa: ${pesoMinore} Kg.</ul>
 `;
 
+// ALTERNATIVA
+let biciLeggera= bike[0];
+for (let i=0; i<bike.length; i++){
+    let bycicle = bike[i];
+    if(bycicle.peso<bicileggera.peso){
+        bicileggera=bike[i];
+    };
+
+};
+
+console.log(bicileggera);
+const {nome, peso}=bicileggera;
 */
 
 /*
@@ -51,7 +63,7 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
 
 */
-
+/*
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
@@ -112,7 +124,76 @@ let newClubs = [];
 for (let z = 0; z < clubs.length; z++) {
     let { nome, falli } = clubs[z];
     newClubs.push({ nome, falli });
-
+    //COme faccio a creare un oggetto ad ogni ciclo?
 };
 console.log(newClubs);
+
+*/
+
+/* SNACK 3
+Scrivere una funzione che accetti tre argomenti, un array e due numeri(a più piccolo di b).
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+*/
+//VECCHIO MODO 
+/*
+let example = (array, a, b) => {
+    if (a < b) {
+        let list = [];
+        for (let i = 0; i < array.length; i++) {
+
+            if (i >= a && i <= b) {
+                list.push(array[i]);
+            };
+        };
+
+        return list;
+    };
+
+};
+
+const matrice = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let a = 4;
+let b = 9;
+let risultato = example(matrice, a, b);
+console.log(risultato);
+*/
+//forEach:
+/*
+let example = (array, a, b) => {
+    if (a < b) {
+        let list = [];
+        array.forEach((element, i, array) => {
+            if (i >= a && i <= b) {
+                list.push(array[i]);
+            };
+        });
+
+        return list;
+    };
+
+};
+const matrice = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let a = 4;
+let b = 9;
+let risultato = example(matrice, a, b);
+console.log(risultato);
+*/
+
+// FILTER:
+let example = (array, a, b) => {
+    if (a < b) {
+        const x = array.filter(function (el) {
+            if (el >= a && el <= b) {
+                return true;
+            };
+            return false;
+        });
+        console.log(x);
+    };
+};
+
+const matrice = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let a = 4;
+let b = 9;
+example(matrice, a, b);
 
